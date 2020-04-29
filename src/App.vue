28b45c2274159b2f908e4a262027b5f426fa76df
naoya00 <bsk470@gmail.com>
2020-04-29 12:51:28 +0900

    add component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,29 @@
 <template>
-  <div>
+  <div class="appLow">
+    <header>
+      <router-link to="/" class="link">Top</router-link>
+      <router-link to="/about" class="link">About</router-link>
+      <router-link to="/production" class="link">Production</router-link>
+    </header>
     <router-view></router-view>
   </div>
 </template>
 
+<style lang="scss" scoped>
 
+.appLow{
+  width: 95%;
+  margin: auto;
+  padding-top: 30px;
+  & .link{
+    margin-right: 30px;
+    text-decoration: none;
+    font-size: 20px;
+    color: #808080;
+    &:hover{
+      color: #000;
+    }
+  }
+}
+
+</style>
--- src/router.js
+++ src/router.js
@@ -2,10 +2,16 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Top from './views/Top.vue';
 import About from './views/About.vue';
+import Production from './views/Production.vue';
+
 
 Vue.use(Router);
 
 export default new Router({
   mode: "history",
-  routes: [{path: "/", component: Top}, {path: "/about", component: About}]
+  routes: [
+    {path: "/", component: Top},
+    {path: "/about", component: About},
+    {path: "/production", component: Production}
+  ]
 })
