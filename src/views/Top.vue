5c0f3089d10fad267a32c266e82c7bf2b9a21d7d
naoya00 <bsk470@gmail.com>
2020-04-30 08:58:53 +0900

    add image

--- src/views/Top.vue
+++ src/views/Top.vue
@@ -1,7 +1,29 @@
 <template>
-  <div>
-    <p>
-      top
-    </p>
+  <div class="topLow">
+    <div class="topLow__texts">
+      Naoya's<br>
+      PortFolio<br>
+      Site
+    </div>
+    <img src="@/assets/user.jpg" />
   </div>
 </template>
+
+<style lang="scss" scoped>
+.topLow{
+  height: 70vh;
+  display: flex;
+  justify-content: space-between;
+  width: 80%;
+  margin: 50px auto;
+  & img{
+    height: 100%;
+  }
+  &__texts{
+    font-family: Courgette,cursive;
+    font-size: 60px;
+    letter-spacing: 1px;
+    color: #808080;
+  }
+}
+</style>
